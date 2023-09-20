b7684135656ce80786f6b7b31da9a445e258a6d2
jacobjp52285 <jpetway522@gmail.com>
2023-09-20 01:24:12 -0500

    Happy Path Tests

--- Clean.Application.Test/FizzBuzzServiceTest.cs
+++ Clean.Application.Test/FizzBuzzServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clean.Application.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,58 @@ using System.Threading.Tasks;
 
 namespace Clean.Application.Test
 {
-    internal class FizzBuzzServiceTest
+    public class FizzBuzzServiceTest
     {
+        private readonly FizzBuzzService _service;
+
+        public FizzBuzzServiceTest(FizzBuzzService service)
+        {
+            _service = service;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(12)]
+        public void SetFizz_ShouldSetDictionary(int number)
+        {
+            // Act
+            _service.SetFizz(number);
+            var list = _service._fizzbuzz.ToList();
+
+            //Assert
+            Assert.True(list.Where(x => x.Value == "fizz").Any());
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(25)]
+        public void SetBuzz_ShouldSetDictionary(int number)
+        {
+            //Act
+            _service.SetBuzz(number);
+            var list = _service._fizzbuzz.ToList();
+            //Assert
+            Assert.True(list.Where(x => x.Value == "buzz").Any());
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        [InlineData(60)]
+        public void SetFizzBuzz_ShouldSetDictionary(int number)
+        {
+            //Act
+            _service.SetFizz(number);
+            _service.SetBuzz(number);
+            
+            var list = _service._fizzbuzz.ToList();
+            //Assert
+            Assert.True(list.Where(x => x.Value == "fizzbuzz").Any());
+        }
     }
 }
